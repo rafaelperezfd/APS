893bdb7c7fbbb8426c3560616afe8a381a7d2048
rafaperezfd <rafaperezlife@gmail.com>
2025-05-12 19:31:25 -0300

    Primeiro commit da APS

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("calcular").addEventListener("click", function () {
+    const valor = parseFloat(document.getElementById("valor").value);
+    const meses = parseInt(document.getElementById("periodo").value);
+    const taxaCDI = parseFloat(document.getElementById("taxa").value) / 100;
+    const rendimentoCDI = parseFloat(document.getElementById("rendimento").value) / 100;
+    
+    const taxaEfetivaAno = taxaCDI * rendimentoCDI;
+    const taxaMensal = Math.pow(1 + taxaEfetivaAno, 1 / 12) - 1;
+    
+    const montante = valor * Math.pow(1 + taxaMensal, meses);
+    const retorno = (montante - valor).toFixed(2);
+    
+    document.getElementById("retorno").textContent = `R$ ${retorno}`;
+});
+  
