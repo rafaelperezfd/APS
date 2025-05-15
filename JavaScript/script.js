4fef0fee86d0918c7f6fae3b023270239ead5d8f
Nicolas <nicolas140917@gmail.com>
2025-05-15 19:01:11 -0300

    ni e alex

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,25 +1,76 @@
-// calculadora de CDI
-document.getElementById("calcular").addEventListener("click", 
-    function () {
-    const valor = parseFloat(document.getElementById("valor").value);
-    const meses = parseInt(document.getElementById("periodo").value);
-    const taxaCDI = parseFloat(document.getElementById("taxa").value) / 100;
-    const rendimentoCDI = parseFloat(document.getElementById("rendimento").value) / 100;
-    
-    const taxaEfetivaAno = taxaCDI * rendimentoCDI;
-    const taxaMensal = Math.pow(1 + taxaEfetivaAno, 1 / 12) - 1;
-    
-    const montante = valor * Math.pow(1 + taxaMensal, meses);
-    const retorno = (montante - valor).toFixed(2);
-    
-    document.getElementById("retorno").textContent = `R$ ${retorno}`;
-});
-  
-// efeito de rolagem
-ScrollReveal().reveal('.reveal', {
-    origin: 'bottom',
-    distance: '50px',
-    duration: 1000,
-    delay: 200,
-    reset: true
-});
+
+  const hamburger = document.getElementById('hamburger');
+  const sideMenu = document.getElementById('sideMenu');
+  const closeBtn = document.getElementById('closeBtn');
+
+  hamburger.addEventListener('click', () => {
+    sideMenu.style.right = '0';
+  });
+
+  closeBtn.addEventListener('click', () => {
+    sideMenu.style.right = '-300px';
+  });
+
+
+
+  // Gráfico de Pizza
+  new Chart(document.getElementById("graficoPizza"), {
+    type: 'pie',
+    data: {
+      labels: ["Renda Fixa", "Ações", "Fundos Imobiliários"],
+      datasets: [{
+        label: "Distribuição de Carteira",
+        backgroundColor: ["#00c767", "#00bfa5", "#00e676"],
+        data: [40, 35, 25]
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+
+  // Gráfico de Barras
+  new Chart(document.getElementById("graficoBarra"), {
+    type: 'bar',
+    data: {
+      labels: ["2021", "2022", "2023"],
+      datasets: [{
+        label: "Investimento de João (R$)",
+        backgroundColor: "#00bfa5",
+        data: [5000, 8500, 12000]
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  // Gráfico de Linha
+  new Chart(document.getElementById("graficoLinha"), {
+    type: 'line',
+    data: {
+      labels: ["2020", "2021", "2022", "2023"],
+      datasets: [{
+        label: "Evolução Patrimonial da Ana (R$)",
+        borderColor: "#00e676",
+        fill: false,
+        data: [1000, 3000, 7000, 15000]
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
